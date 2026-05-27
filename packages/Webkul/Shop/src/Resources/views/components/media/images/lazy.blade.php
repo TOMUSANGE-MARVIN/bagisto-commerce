2cb82bdc76b8cbfb4b92751abb300c44a4071793
TOMUSANGE-MARVIN <marvintomusange@gmail.com>
2026-05-27 11:35:11 +0300

    Revert "Fix lazy image component to use src directly instead of data-src"
    This reverts commit cd352f4fa5855145174fd24fe00af81ad9a9bf90.

--- packages/Webkul/Shop/src/Resources/views/components/media/images/lazy.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/media/images/lazy.blade.php
@@ -17,7 +17,7 @@
 
         <img
             v-bind="$attrs"
-            :src="src"
+            :data-src="src"
             :id="'image-' + $.uid"
             @load="onLoad"
             v-show="! isLoading"
@@ -26,7 +26,7 @@
 
         <img
             v-bind="$attrs"
-            :src="src"
+            :data-src="src"
             :id="'image-' + $.uid"
             @load="onLoad"
             v-else
